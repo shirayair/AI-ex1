226a8e816218c2aff3b66a5167ce77054e675b27
Michael <michaelitzhaki@gmail.com>
2020-11-26 03:04:22 +0200

    Fix shira queen

--- Algorithms/IterativeDeepeningDFS.py
+++ Algorithms/IterativeDeepeningDFS.py
@@ -9,22 +9,21 @@ class IterativeDeepeningDFS(SearchProblem.SearchProblemSolver):
 
     def solve(self):
         for max_depth in range(1, 20):
+            self.problem.num_of_nodes += 1
             res = self._dfs_search(self.start_node, max_depth)
             if res:
-                return  res
+                return res
 
         return None
 
     def _dfs_search(self, start_node: Node.Node, max_depth: int):
         if self.problem.is_goal(start_node.state):
             return start_node
-        if start_node.depth > max_depth:
+        if start_node.depth >= max_depth:
             return
 
-        open_ls = self.expand(start_node)
-        while open_ls:
-            n = open_ls.pop()
+        for child in self.expand(start_node):
             self.problem.num_of_nodes += 1
-            result = self._dfs_search(n, max_depth)
+            result = self._dfs_search(child, max_depth)
             if result:
                 return result
